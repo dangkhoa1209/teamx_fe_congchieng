e4522aa68c0c829e47f09918b1b971bbba191002
Dang Khoa <hdkhoa.work@gmail.com>
2025-11-29 15:20:59 +0700

    ok

--- src/layouts/default/footer/index.vue
+++ src/layouts/default/footer/index.vue
@@ -18,7 +18,7 @@
                 </p>
               </div>
             </div>
-            <p class="font-robo font-normal text-body">
+            <p class="font-normal text-body">
               Chuyên trang thông tin điện tử giới thiệu, quảng bá mô hình văn hoá cồng chiêng gắn
               với phát triển du lịch tỉnh Lâm Đồng
             </p>
@@ -44,24 +44,38 @@
           </div>
         </div>
         <div class="w-[25%] flex flex-col gap-6">
-          <p class="font-robo font-medium text-subtitle">Danh mục</p>
-          <p class="font-robo font-normal text-boby">Giới thiệu về chuyên trang</p>
-          <p class="font-robo font-normal text-boby">Tiềm năng phát triển</p>
-          <p class="font-robo font-normal text-boby">Văn hóa cồng chiêng</p>
-          <p class="font-robo font-normal text-boby">Tin tức - Sự kiện</p>
-          <p class="font-robo font-normal text-boby">Hợp tác</p>
+          <p class="font-medium text-subtitle">DANH MỤC</p>
+          <p class="font-normal text-boby cursor-pointer" @click="clickTo('gioi-thieu')">
+            Giới thiệu về chuyên trang
+          </p>
+          <p
+            class="font-normal text-boby cursor-pointer"
+            @click="clickTo('tiem-nang-phat-trien/van-hoa-nghe-thuat')"
+          >
+            Tiềm năng phát triển
+          </p>
+          <p
+            class="font-normal text-boby cursor-pointer"
+            @click="clickTo('van-hoa-cong-chieng/tong-quan')"
+          >
+            Văn hóa cồng chiêng
+          </p>
+          <p class="font-normal text-boby cursor-pointer" @click="clickTo('tin-tuc-su-kien')">
+            Tin tức - Sự kiện
+          </p>
+          <p class="font-normal text-boby cursor-pointer" @click="clickTo('hop-tac')">Hợp tác</p>
         </div>
         <div class="w-[35%] flex flex-col gap-6">
-          <p class="font-robo font-medium text-subtitle">LIÊN HỆ</p>
-          <p class="font-robo font-normal text-boby">
+          <p class="font-medium text-subtitle">LIÊN HỆ</p>
+          <p class="font-normal text-boby">
             Ban Biên tập Chuyên Trang thông tin điện tử - Sở Văn hóa, Thể thao và Du lịch tỉnh Lâm
             Đồng
           </p>
-          <p class="font-robo font-normal text-boby">
+          <p class="font-normal text-boby">
             Địa chỉ: Tầng 2 khu 9 tầng, Số 36 Trần Phú, Phường Xuân Hương Đà Lạt - Tỉnh Lâm Đồng
           </p>
-          <p class="font-robo font-normal text-boby">Số điện thoại: (0263) 3822141</p>
-          <p class="font-robo font-normal text-boby">Email: svhttdl.lamdong.gov.vn</p>
+          <p class="font-normal text-boby">Số điện thoại: (0263) 3822141</p>
+          <p class="font-normal text-boby">Email: svhttdl.lamdong.gov.vn</p>
         </div>
       </div>
       <x-space :height="58" />
@@ -69,7 +83,7 @@
 
       <div class="py-8 flex justify-center">
         <p
-          class="font-robo font-medium text-subtitle text-center inline-flex items-center justify-center gap-1"
+          class="font-medium text-subtitle text-center inline-flex items-center justify-center gap-1"
         >
           <span class="text-[25px] leading-none">©</span>
           Bản quyền thuộc Sở Văn Hoá, Thể thao & Du lịch tỉnh Lâm Đồng
@@ -82,4 +96,10 @@
   import FBIcon from '~/public/assets/icon/fb.svg';
   import YTIcon from '~/public/assets/icon/youtube.svg';
   import IGIcon from '~/public/assets/icon/ig.svg';
+
+  const router = useRouter();
+  const clickTo = (slugfy) => {
+    if (!slugfy) return;
+    router.push({ path: `/${slugfy}` });
+  };
 </script>
