644efcd73f661b389979e10cd4d79f55440c8a08
Dang Khoa <hdkhoa.work@gmail.com>
2025-12-05 00:59:39 +0700

    gửi mail

--- src/composables/$url.js
+++ src/composables/$url.js
@@ -8,12 +8,14 @@ import imagePageAdmin from './admin-url/image-page.js';
 import news from './url/news.js';
 import featuredNews from './url/featured-news.js';
 import imagePage from './url/image-page.js';
+import contact from './url/contact.js';
 
 export default {
   auth,
   news,
   featuredNews,
   image_page: imagePage,
+  contact,
   admin: {
     news: newsAdmin,
     account: accountAdmin,
